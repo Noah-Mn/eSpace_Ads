6dd2f278208292019877f405b7c1db428f7df829
Noah-Mn <bsc-com-09-17@unima.ac.mw>
2022-05-11 11:03:04 +0200

    init

---
+++ app/src/main/java/com/example/espace_ads/activityClasses/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.espace_ads.activityClasses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.espace_ads.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
