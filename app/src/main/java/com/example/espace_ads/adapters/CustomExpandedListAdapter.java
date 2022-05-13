c89c792a5596c686cba656533ddc2b3d8d71eb27
BSC_COM_09_17 <bsc-com-09-17@unima.ac.mw>
2022-05-13 01:38:35 -0800

    lists

---
+++ app/src/main/java/com/example/espace_ads/adapters/CustomExpandedListAdapter.java
@@ -0,0 +1,83 @@
+package com.example.espace_ads.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+
+import com.example.espace_ads.R;
+import com.google.android.material.textview.MaterialTextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class CustomExpandedListAdapter extends BaseExpandableListAdapter {
+
+    private Context context;
+    private List<String> expandableListTitle;
+    private HashMap<String, List<String>> expandableListDetails;
+
+    public CustomExpandedListAdapter(Context context, List<String> expandableListTitle, HashMap<String, List<String>> expandableListDetails) {
+        this.context = context;
+        this.expandableListTitle = expandableListTitle;
+        this.expandableListDetails = expandableListDetails;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return 0;
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return 0;
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return null;
+    }
+
+    @Override
+    public Object getChild(int listPosition, int expandedListPosition) {
+        return this.expandableListDetails.get(this.expandableListTitle.get(listPosition)).get(expandedListPosition);
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int listPosition, int expandedListPosition) {
+        return expandedListPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public View getChildView(int listPosition, int expandedListPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        final String expandedListText = (String) getChild(listPosition, expandedListPosition);
+        if (convertView == null){
+            LayoutInflater layoutInflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.list_item, null);
+        }
+        MaterialTextView expandedListTextView = (MaterialTextView) convertView.findViewById(R.id.expanded_list);
+        expandedListTextView.setText(expandedListText);
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return false;
+    }
+}
