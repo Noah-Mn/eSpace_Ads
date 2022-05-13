ee80a2fed28e50f7f127d6f185c31703360c232d
BSC_COM_09_17 <bsc-com-09-17@unima.ac.mw>
2022-05-13 04:14:41 -0800

    geo stati

--- app/src/main/java/com/example/espace_ads/fragments/Statistics.java
+++ app/src/main/java/com/example/espace_ads/fragments/Statistics.java
@@ -12,7 +12,6 @@ import android.widget.ExpandableListView;
 
 import com.example.espace_ads.R;
 import com.example.espace_ads.adapters.CustomExpandedListAdapter;
-import com.example.espace_ads.data.ExpandableListDataPump;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -36,10 +35,52 @@ public class Statistics extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_statistics, container, false);
         expandableListView = view.findViewById(R.id.expandable_list_view);
-        expandableListDetail = ExpandableListDataPump.getData();
-        expandableListTitle = new ArrayList<>(expandableListDetail.keySet());
+
+
+//        inflate continents
+        expandableListTitle = new ArrayList<>();
+        expandableListDetail = new HashMap<>();
+
+        expandableListTitle.add("Africa");
+        expandableListTitle.add("America");
+        expandableListTitle.add("Europe");
+        expandableListTitle.add("Asia");
+        expandableListTitle.add("Australia");
+
+        List<String> africa = new ArrayList<>();
+        africa.add("Malawi");
+        africa.add("Zambia");
+        africa.add("Namibia");
+
+        List<String> america = new ArrayList<>();
+        america.add("Malawi");
+        america.add("Zambia");
+        america.add("Namibia");
+
+        List<String> europe = new ArrayList<>();
+        europe.add("Malawi");
+        europe.add("Zambia");
+        europe.add("Namibia");
+
+        List<String> asia = new ArrayList<>();
+        asia.add("Malawi");
+        asia.add("Zambia");
+        asia.add("Namibia");
+
+        List<String> australia = new ArrayList<>();
+        australia.add("Malawi");
+        australia.add("Zambia");
+        australia.add("Namibia");
+
+        expandableListDetail.put(expandableListTitle.get(0), africa);
+        expandableListDetail.put(expandableListTitle.get(1), america);
+        expandableListDetail.put(expandableListTitle.get(2), europe);
+        expandableListDetail.put(expandableListTitle.get(3), asia);
+        expandableListDetail.put(expandableListTitle.get(4), australia);
+
         expandableListAdapter = new CustomExpandedListAdapter(getContext(), expandableListTitle, expandableListDetail);
         expandableListView.setAdapter(expandableListAdapter);
+
         return view;
     }
 }
