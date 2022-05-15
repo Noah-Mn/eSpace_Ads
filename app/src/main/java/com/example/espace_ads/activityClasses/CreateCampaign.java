2c4c5757d8777796db787016057347b0463ed49c
Noah-Mn <bsc-com-09-17@unima.ac.mw>
2022-05-15 12:25:02 +0200

    create campaign

---
+++ app/src/main/java/com/example/espace_ads/activityClasses/CreateCampaign.java
@@ -0,0 +1,14 @@
+package com.example.espace_ads.activityClasses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateCampaign extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_campaign);
+    }
+}
