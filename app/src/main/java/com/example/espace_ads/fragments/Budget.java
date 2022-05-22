921969d0e1aa49bb8230bb58934ae62e8f17c51f
noah <bsc-com@unima.ac.mw>
2022-05-22 21:05:08 +0200

    backend

--- app/src/main/java/com/example/espace_ads/fragments/Budget.java
+++ app/src/main/java/com/example/espace_ads/fragments/Budget.java
@@ -1,13 +1,19 @@
 package com.example.espace_ads.fragments;
 
+import android.app.TimePickerDialog;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.espace_ads.R;
@@ -15,14 +21,24 @@ import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.textfield.TextInputEditText;
 import com.jaredrummler.materialspinner.MaterialSpinner;
 
+import org.joda.time.Days;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Objects;
 
 public class Budget extends Fragment {
 
     MaterialSpinner frequencySp, amountSp, amountPerViewSp;
-    TextInputEditText customAmount;
-    MaterialCardView custom;
+    TextInputEditText customAmount, startTime, endTime, startDate, endDate;
+    MaterialCardView custom, btnCalculate;
+    private Calendar calendar;
+    private int currentHour;
+    private int currentMinute;
+    private TimePickerDialog timePickerDialog;
+    String startingDate, endingDate, startingTime, endingTime;
 
     public Budget() {
         // Required empty public constructor
@@ -45,10 +61,26 @@ public class Budget extends Fragment {
         amountPerViewSp = view.findViewById(R.id.spinner_amount_per_view);
         customAmount = view.findViewById(R.id.custom_amount);
         custom = view.findViewById(R.id.custom);
+        startTime = view.findViewById(R.id.editText_time);
+        endTime = view.findViewById(R.id.editText_end_time);
+        btnCalculate = view.findViewById(R.id.calculate_btn);
 
         setAmountSpinner(view);
         setFrequencySpinner(view);
         setAmountPerViewSpinner(view);
+        loadTimePicker();
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    calculate();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         return view;
     }
 
@@ -65,7 +97,7 @@ public class Budget extends Fragment {
 
                 if (position == amountValues.length - 1) {
                     custom.setVisibility(View.VISIBLE);
-                   long value = Integer.parseInt(Objects.requireNonNull(customAmount.getText()).toString());
+//                    int value = Integer.parseInt(customAmount.getText().toString());
 
 //                   will set value here
                 } else {
@@ -77,6 +109,7 @@ public class Budget extends Fragment {
 
         });
     }
+
 
     public void setFrequencySpinner(View view) {
 
@@ -103,5 +136,79 @@ public class Budget extends Fragment {
         ArrayAdapter<String> amountPerViewAdapter = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_spinner_item, amountPerView);
         amountPerViewAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         amountPerViewSp.setAdapter(amountPerViewAdapter);
+    }
+
+    public void loadTimePicker() {
+        disableSoftInputFromAppearing(startTime);
+        startTime.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+
+                calendar = Calendar.getInstance();
+                currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+                currentMinute = calendar.get(Calendar.MINUTE);
+
+                timePickerDialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                        startTime.setText(String.format("%02d : %02d", hourOfDay, minutes));
+                    }
+                }, currentHour, currentMinute, false);
+
+                timePickerDialog.show();
+            }
+        });
+
+        disableSoftInputFromAppearing(endTime);
+        endTime.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+
+                calendar = Calendar.getInstance();
+                currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+                currentMinute = calendar.get(Calendar.MINUTE);
+
+                timePickerDialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                        endTime.setText(String.format("%02d : %02d", hourOfDay, minutes));
+                    }
+                }, currentHour, currentMinute, false);
+
+                timePickerDialog.show();
+            }
+        });
+
+    }
+
+    public static void disableSoftInputFromAppearing(TextInputEditText pTime) {
+        if (Build.VERSION.SDK_INT >= 11) {
+            pTime.setRawInputType(InputType.TYPE_NULL);
+            pTime.setTextIsSelectable(true);
+        } else {
+            pTime.setRawInputType(InputType.TYPE_NULL);
+            pTime.setFocusable(true);
+        }
+    }
+    public void getInfo(){
+        startingTime = startTime.getText().toString();
+        endingTime = endTime.getText().toString();
+    }
+    public long calculate() throws ParseException {
+        getInfo();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Date date1 = sdf.parse(startingDate);
+        Date date2 = sdf.parse(endingDate);
+
+        assert date1 != null;
+        assert date2 != null;
+        long numDays = date1.getTime()- date2.getTime();
+
+        Toast.makeText(getContext(), "Days" + numDays, Toast.LENGTH_SHORT).show();
+
+        return numDays;
+
     }
 }
