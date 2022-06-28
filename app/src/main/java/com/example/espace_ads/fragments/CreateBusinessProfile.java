bc0f94ef93ce581238b417ce6277dc64a08d7065
noah <bsc-com@unima.ac.mw>
2022-06-28 16:15:32 +0200

    business profile done

--- app/src/main/java/com/example/espace_ads/fragments/CreateBusinessProfile.java
+++ app/src/main/java/com/example/espace_ads/fragments/CreateBusinessProfile.java
@@ -2,6 +2,7 @@ package com.example.espace_ads.fragments;
 
 import static android.app.Activity.RESULT_OK;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
@@ -9,25 +10,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.webkit.MimeTypeMap;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.espace_ads.R;
-import com.example.espace_ads.adapters.GridAdapter;
 import com.example.espace_ads.models.ItemsModel;
-import com.example.espace_ads.models.Upload;
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.checkbox.MaterialCheckBox;
@@ -39,26 +39,37 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 import com.makeramen.roundedimageview.RoundedImageView;
+import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class CreateBusinessProfile extends Fragment {
     View view;
     LinearLayout socialMediaProfiles, socialMedia;
     TextInputEditText textCompanyName, textCompanyType, textCompanyDescription, companyURL, editTextFacebook, editTextTwitter, editTextInstagram, editTextLinkedin;
-    MaterialCardView addProfileLinks, addItems;
-    ImageView imageView;
+    MaterialCardView saveBusinessProfile, addProfileLinks;
+    AppCompatImageView imageView;
     MaterialCheckBox facebook, twitter, instagram, linkedin;
-    FirebaseFirestore db;
+    FirebaseFirestore firebaseFirestore;
     FirebaseUser currentUser;
-    private RoundedImageView roundedImageView, coverImage, companyLogo;
+    private RoundedImageView roundedImageView, coverImage, companyLogoImage;
     private final int PICK_COVER_REQUEST = 1;
     private final int PICK_LOGO_REQUEST = 2;
-    ArrayList<ItemsModel> items = new ArrayList<>();
     private ProgressBar mProgressBar;
-    private ArrayList<Upload> mGridData;
-//    private final String FEED_URL = "http://javatechig.com/?json=get_recent_posts&count=45";
+    private StorageTask<UploadTask.TaskSnapshot> uploadTask, uploadTask2;
+    DatabaseReference reference;
+    Uri coverImagePath, logoPath;
+    StorageReference storageReference;
 
 
     public CreateBusinessProfile() {
@@ -78,30 +89,30 @@ public class CreateBusinessProfile extends Fragment {
         socialMedia = view.findViewById(R.id.social_media);
         socialMediaProfiles = view.findViewById(R.id.social_media_profiles);
         coverImage = view.findViewById(R.id.cover_image);
-        companyLogo = view.findViewById(R.id.add_logo);
+        companyLogoImage = view.findViewById(R.id.add_logo);
         textCompanyName = view.findViewById(R.id.text_company_name);
         textCompanyType = view.findViewById(R.id.text_company_type);
         textCompanyDescription = view.findViewById(R.id.company_description);
         companyURL = view.findViewById(R.id.company_url);
-        addProfileLinks = view.findViewById(R.id.add_profile_links);
+        saveBusinessProfile = view.findViewById(R.id.save_business_pro);
         imageView = view.findViewById(R.id.cover_pic);
         roundedImageView = view.findViewById(R.id.image_logo);
         facebook = view.findViewById(R.id.check_facebook);
         instagram = view.findViewById(R.id.check_instagram);
         twitter = view.findViewById(R.id.check_twitter);
         linkedin = view.findViewById(R.id.check_linkedin);
+        addProfileLinks = view.findViewById(R.id.add_profile_links);
         editTextFacebook = view.findViewById(R.id.editText_facebook);
         editTextInstagram = view.findViewById(R.id.editText_instagram);
         editTextLinkedin = view.findViewById(R.id.editText_linkedin);
         editTextTwitter = view.findViewById(R.id.editText_twitter);
-        addItems = view.findViewById(R.id.add_items);
         mProgressBar = view.findViewById(R.id.progressBar);
-        db = FirebaseFirestore.getInstance();
-
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference("Business Profile Images").child(getEmail());
 
         listeners();
 
-        db.collection("Store Items")
+        firebaseFirestore.collection("Business Profile")
                 .whereEqualTo("Email Address", getEmail())
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -111,21 +122,37 @@ public class CreateBusinessProfile extends Fragment {
                         if (task.isSuccessful() && task.getResult() != null) {
 
                             for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
-                                String name = documentSnapshot.getString("Item Name");
-                                int price = Math.toIntExact(documentSnapshot.getLong("Item Price"));
-                                String description = documentSnapshot.getString("Item Description");
-                                String downloadUri = documentSnapshot.getString("DownloadUri");
-                                ItemsModel itemsModel = new ItemsModel(name, price, description, downloadUri);
-                                items.add(itemsModel);
+                                String companyName = documentSnapshot.getString("Company Name");
+                                String description = documentSnapshot.getString("Company Bio");
+                                String companyType = documentSnapshot.getString("Company Type");
+                                String companyUrl = documentSnapshot.getString("Company Url");
+                                String companyCoverImage = documentSnapshot.getString("CoverImageUri");
+                                String companyLogo = documentSnapshot.getString("LogoUri");
+
+                                if (companyName != null && description != null && companyType != null && companyUrl != null && companyCoverImage != null && companyLogo != null){
+
+                                    try {
+                                        URL coverUrl = new URL(companyCoverImage);
+                                        Picasso.with(getContext()).load(String.valueOf(coverUrl)).into(imageView);
+                                    } catch (MalformedURLException e) {
+                                        e.printStackTrace();
+                                    }
+                                    try {
+                                        URL logoUrl = new URL(companyLogo);
+                                        Picasso.with(getContext()).load(String.valueOf(logoUrl)).into(roundedImageView);
+                                    } catch (MalformedURLException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                    textCompanyName.setText(companyName);
+                                    textCompanyType.setText(companyType);
+                                    textCompanyDescription.setText(description);
+                                    companyURL.setText(companyUrl);
+
+                                }
+
                             }
-                            if (items.size() > 0) {
-//                                gridAdapter.setStoreItemsList(items);
-                                mProgressBar.setVisibility(View.GONE);
-//                                liveCampaign.setVisibility(View.GONE);
-                            } else {
-//                                liveCampaign.setVisibility(View.VISIBLE);
-                                mProgressBar.setVisibility(View.GONE);
-                            }
+
                         } else {
                             Toast.makeText(getContext(), "Failed to get data", Toast.LENGTH_SHORT).show();
 
@@ -134,21 +161,15 @@ public class CreateBusinessProfile extends Fragment {
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getContext(), "Failed to get data", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Error: "+e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//
-//            }
-//        });
+
         return view;
     }
 
     private void listeners() {
-        
+
         coverImage.setColorFilter(Color.WHITE);
         coverImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -157,8 +178,8 @@ public class CreateBusinessProfile extends Fragment {
             }
         });
 
-        companyLogo.setColorFilter(Color.WHITE);
-        companyLogo.setOnClickListener(new View.OnClickListener() {
+        companyLogoImage.setColorFilter(Color.WHITE);
+        companyLogoImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 chooseLogo();
@@ -216,10 +237,11 @@ public class CreateBusinessProfile extends Fragment {
             }
         });
 
-        addItems.setOnClickListener(new View.OnClickListener() {
+        saveBusinessProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                replaceFragments(new AddItemToStore());
+                setItemsToDatabase();
+                displaySavedData();
             }
         });
     }
@@ -243,12 +265,12 @@ public class CreateBusinessProfile extends Fragment {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == PICK_COVER_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            Uri coverImagePath = data.getData();
+            coverImagePath = data.getData();
             imageView.setImageURI(coverImagePath);
         }
 
         if (requestCode == PICK_LOGO_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            Uri logoPath = data.getData();
+            logoPath = data.getData();
             roundedImageView.setImageURI(logoPath);
         }
     }
@@ -265,9 +287,149 @@ public class CreateBusinessProfile extends Fragment {
     public String getEmail() {
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
         String emailAddress;
+        assert currentUser != null;
         emailAddress = currentUser.getEmail();
         return emailAddress;
     }
 
+    private String getFileExtension(Uri uri) {
+        ContentResolver contentResolver = (getActivity()).getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
 
+    private void setItemsToDatabase() {
+
+        String companyName = Objects.requireNonNull(textCompanyName.getText()).toString().trim();
+        String companyType = Objects.requireNonNull(textCompanyType.getText()).toString().trim();
+        String companyBio = Objects.requireNonNull(textCompanyDescription.getText()).toString().trim();
+        String companyUrl = Objects.requireNonNull(companyURL.getText()).toString().trim();
+        String textFacebook = Objects.requireNonNull(editTextFacebook.getText()).toString().trim();
+        String textInstagram = Objects.requireNonNull(editTextInstagram.getText()).toString().trim();
+        String textLinkedin = Objects.requireNonNull(editTextLinkedin.getText()).toString().trim();
+        String textTwitter = Objects.requireNonNull(editTextTwitter.getText()).toString().trim();
+
+
+        reference = FirebaseDatabase.getInstance().getReference("Business Profile");
+        if (coverImagePath != null && logoPath != null) {
+            StorageReference fileReference = storageReference.child(System.currentTimeMillis() + "." + getFileExtension(coverImagePath));
+            StorageReference fileReference2 = storageReference.child(System.currentTimeMillis() + "." + getFileExtension(logoPath));
+
+            uploadTask = fileReference.putFile(coverImagePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                            Handler handler = new Handler();
+//                            handler.postDelayed(new Runnable() {
+//                                @Override
+//                                public void run() {
+////                                    progressBar.setProgress(0);
+////                                    progressBar.setVisibility(View.GONE);
+////                                    replaceFragments(new BusinessProfileStore());
+//                                }
+//                            }, 500);
+//
+//                            String uploadId = reference.push().getKey();
+//                            reference.child(uploadId).setValue(upload);
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                            double progress = (100.0 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+//                            progressBar.setProgress((int) progress);
+                        }
+                    });
+
+            uploadTask2 = fileReference2.putFile(logoPath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getContext(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task task) throws Exception {
+                    if (!task.isComplete()) {
+                        throw Objects.requireNonNull(task.getException());
+                    }
+                    return fileReference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        Uri downloadUri = task.getResult();
+
+                        uploadTask2.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                            @Override
+                            public Task<Uri> then(@NonNull Task task) throws Exception {
+                                if (!task.isComplete()) {
+                                    throw Objects.requireNonNull(task.getException());
+                                }
+                                return fileReference2.getDownloadUrl();
+                            }
+                        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Uri> task) {
+                                if (task.isSuccessful()) {
+                                    Uri logoDownloadUri = task.getResult();
+
+                                    firebaseFirestore = FirebaseFirestore.getInstance();
+                                    Map<String, Object> Item = new HashMap<>();
+
+                                    Item.put("Company Name", companyName);
+                                    Item.put("Company Type", companyType);
+                                    Item.put("Company Bio", companyBio);
+                                    Item.put("Company Url", companyUrl);
+                                    Item.put("Email Address", getEmail());
+                                    Item.put("CoverImageUri", downloadUri);
+                                    Item.put("LogoUri", logoDownloadUri);
+                                    Item.put("Facebook Profile", textFacebook);
+                                    Item.put("Instagram Profile", textInstagram);
+                                    Item.put("Linkedin Profile", textLinkedin);
+                                    Item.put("Twitter Profile", textTwitter);
+                                    Item.put("Visits", 0L);
+                                    Item.put("Engagements", 0L);
+                                    Item.put("Sells", 0L);
+
+                                    firebaseFirestore.collection("Business Profile")
+                                            .add(Item)
+                                            .addOnSuccessListener(documentReference -> Toast.makeText(getContext(), "Data has been saved", Toast.LENGTH_SHORT).show())
+                                            .addOnFailureListener(e -> Toast.makeText(getContext(), "Failed to save data", Toast.LENGTH_SHORT).show());
+
+                                } else {
+                                    Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+                    } else {
+                        Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(getContext(), "No file selected!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void displaySavedData() {
+
+    }
 }
---
+++ app/src/main/java/com/example/espace_ads/models/BusinessProfileModel.java
@@ -0,0 +1,110 @@
+package com.example.espace_ads.models;
+
+public class BusinessProfileModel {
+    String companyName, companyBio, reviewIndividuals, companyType, email, socialMedia, companyUrl, companyLogo, companyCoverImage;
+    long reviewsNumber,engagements, sells, visits;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyBio() {
+        return companyBio;
+    }
+
+    public void setCompanyBio(String companyBio) {
+        this.companyBio = companyBio;
+    }
+
+    public String getReviewIndividuals() {
+        return reviewIndividuals;
+    }
+
+    public void setReviewIndividuals(String reviewIndividuals) {
+        this.reviewIndividuals = reviewIndividuals;
+    }
+
+    public String getCompanyType() {
+        return companyType;
+    }
+
+    public void setCompanyType(String companyType) {
+        this.companyType = companyType;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSocialMedia() {
+        return socialMedia;
+    }
+
+    public void setSocialMedia(String socialMedia) {
+        this.socialMedia = socialMedia;
+    }
+
+    public String getCompanyUrl() {
+        return companyUrl;
+    }
+
+    public void setCompanyUrl(String companyUrl) {
+        this.companyUrl = companyUrl;
+    }
+
+    public String getCompanyLogo() {
+        return companyLogo;
+    }
+
+    public void setCompanyLogo(String companyLogo) {
+        this.companyLogo = companyLogo;
+    }
+
+    public String getCompanyCoverImage() {
+        return companyCoverImage;
+    }
+
+    public void setCompanyCoverImage(String companyCoverImage) {
+        this.companyCoverImage = companyCoverImage;
+    }
+
+    public long getReviewsNumber() {
+        return reviewsNumber;
+    }
+
+    public void setReviewsNumber(long reviewsNumber) {
+        this.reviewsNumber = reviewsNumber;
+    }
+
+    public long getEngagements() {
+        return engagements;
+    }
+
+    public void setEngagements(long engagements) {
+        this.engagements = engagements;
+    }
+
+    public long getSells() {
+        return sells;
+    }
+
+    public void setSells(long sells) {
+        this.sells = sells;
+    }
+
+    public long getVisits() {
+        return visits;
+    }
+
+    public void setVisits(long visits) {
+        this.visits = visits;
+    }
+}
